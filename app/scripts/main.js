ca6c94032d2b1c04ba84406b24e255ba6a741a47
Anthony Chavez <artisgarbage@gmail.com>
2016-02-24 00:08:14 -0700

    Confirmation message and UI

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,19 @@
-console.log('main.js');
+'use strict';
+
+$( function() {
+
+    $( '#confirmationModal' ).hide();
+
+    function showModal ( messageStr ) {
+        $( '#confirmationTxt' ).text( messageStr );
+        $( '#confirmationModal' ).fadeIn( 300 ).delay( 1000 ).fadeOut( 300 );
+    }
+
+    $( '#coming' ).on( 'click', function() {
+        showModal( 'Great, see you soon!' );
+    } );
+
+    $( '#notComing' ).on( 'click', function() {
+        showModal( 'No problem, thanks for the RSVP!' );
+    } );
+} );
